a374299ca86ea9b2d9923c6a97dc360e4862a7a8
Danielle Adams <danielle.adams3@extendhealth.com>
2022-10-31 12:20:44 -0600

    ARCH-1619 - Cleanup tech debt & old patterns
    - Recompile using npm 8.15.0 and node v16.17.1
    - Verify required args in action.yml are handled correctly in action code
    - Ensure required args use the required options in getInput()

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 const core = require('@actions/core');
 
-const authorizedUsersInput = core.getInput('authorized-actors').toLowerCase();
+// When used, this requiredArgOptions will cause the action to error if a value has not been provided.
+const requiredArgOptions = {
+  required: true,
+  trimWhitespace: true
+};
+
+const authorizedUsersInput = core.getInput('authorized-actors', requiredArgOptions).toLowerCase();
 const authorizedUsers = JSON.parse(authorizedUsersInput);
 core.info(`The authorized actors for the workflow are: \n- ${authorizedUsers.join('\n- ')}`);
 
-const actor = core.getInput('actor');
+const actor = core.getInput('actor', requiredArgOptions);
 core.info(`The current actor is ${actor}`);
 
 let index = authorizedUsers.indexOf(actor.toLowerCase());
